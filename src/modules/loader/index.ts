dd7e937ee2b7df80cfb060d3be534d96cd526008
Abraxas-365 <luisfmiranda8@gmail.com>
2024-01-09 00:07:24 -0500

    refactor(loader): add export to index

---
+++ src/modules/loader/index.ts
@@ -0,0 +1,7 @@
+export { Service } from "./service";
+export { Repository, TextParcer } from "./ports";
+export {
+  S3TextParser,
+  PdfTextParser,
+  PostgresRepository,
+} from "./adapter/index";
